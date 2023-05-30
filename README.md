2ed98037147fa7d28110ce5f2605202cd2bcfc40
Vortex <xvscode@gmail.com>
2023-05-30 17:46:50 +0800

    refactor: add k8s and hadoop

--- README.md
+++ README.md
@@ -12,7 +12,10 @@ A curated list of awesome resources, plugins, and tools for Vagrant.
 - [Vagrant GitHub Repository](https://github.com/hashicorp/vagrant)
 
 
-## Contribution
+## Contributors
+I would like to express our gratitude to the following contributors for their valuable contributions to this project:
+
+- [罗晓俊](https://github.com/luoxiaojun1992) - Implemented hadoop vagrantfile code.
 
 Contributions are welcome! Please fork the repository and submit a pull request.
 
--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -6,19 +6,26 @@ function setup_docker() {
     return 0
   fi
 
-  local country=$(curl -sSL https://ipapi.co/country)
+  local access_google=false
+  local url="google.com"
+  local ping_output=$(ping -c 1 "$url" 2>&1)
+  # 检查ping命令的退出状态码和ping_output的内容
+  if [ $? -eq 0 ] && [[ $ping_output == *"1 packets transmitted, 1 received"* ]]; then
+      access_google=true
+  fi
+
   info "Installing Docker..."
   curl -fsSL https://get.docker.com -o get-docker.sh
-  if [ "$country" = "CN" ]; then
+  if [ ! "$access_google" ]; then
     sudo sh get-docker.sh --mirror Aliyun
   else
     sudo sh get-docker.sh
   fi
 
-  rm get-docker.sh
+  sudo rm get-docker.sh
   info "Docker installed successfully."
 
-  if [ "$country" = "CN" ]; then
+  if [ ! "$access_google" ]; then
     info "Configuring Docker registry mirrors..."
     if [ ! -d "/etc/docker" ]; then
         sudo mkdir -p /etc/docker
@@ -27,7 +34,7 @@ function setup_docker() {
     {
       "registry-mirrors": ["https://gwsg6nw9.mirror.aliyuncs.com"]
     }
-  EOF
+    EOF
     sudo systemctl daemon-reload
     sudo systemctl restart docker
     info "Docker registry mirrors configured successfully."
